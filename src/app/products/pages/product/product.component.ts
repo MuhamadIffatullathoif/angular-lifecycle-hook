7b0e1b590481556757d8fbade4488edab74399a7
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-21 09:40:23 +0700

    02. Implement All Lifecycle Hook

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component, DoCheck,
+  OnChanges, OnDestroy,
+  OnInit, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  public isProductVisible: boolean = false;
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({changes})
+    console.log('ngOnChanges');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
 
 }
